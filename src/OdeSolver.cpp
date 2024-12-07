e6b0647f2bc577bb9517e85376e5a43c08b76f73
robinfaro <127199925+robinfaro@users.noreply.github.com>
2024-12-07 19:14:59 +0100

    fix loop and small bugs

--- src/OdeSolver.cpp
+++ src/OdeSolver.cpp
@@ -6,6 +6,10 @@ OdeSolver::OdeSolver()
 
 OdeSolver::OdeSolver(double step_size, double initial_time, double final_time, Eigen::MatrixXd initial_condition, Function function)
 {   
+    if (step_size <= 0)
+        throw std::invalid_argument("Step size must be positive");
+    if (initial_time >= final_time)
+        throw std::invalid_argument("Initial time must be smaller than final time");
     this->step_size = step_size;
     this->initial_time = initial_time;
     this->final_time = final_time;
@@ -20,11 +24,15 @@ OdeSolver::~OdeSolver()
 
 void OdeSolver::SetStepSize(double step_size)
 {
+    if (step_size <= 0)
+        throw std::invalid_argument("Step size must be positive");
     this->step_size = step_size;
 }
 
 void OdeSolver::SetTimeInterval(double initial_time, double final_time)
 {
+    if (initial_time >= final_time)
+        throw std::invalid_argument("Initial time must be smaller than final time");
     this->initial_time = initial_time;
     this->final_time = final_time;
 }
--- src/RungeKutta.cpp
+++ src/RungeKutta.cpp
@@ -1,4 +1,5 @@
 #include "RungeKutta.h"
+#include "utils.h"
 
 RungeKutta::RungeKutta()
 {
@@ -7,6 +8,10 @@ RungeKutta::RungeKutta()
 
 RungeKutta::RungeKutta(double step_size, double initial_time, double final_time, Eigen::MatrixXd initial_condition, Function function, Eigen::MatrixXd a, Eigen::VectorXd b, Eigen::VectorXd c) : OdeSolver(step_size, initial_time, final_time, initial_condition, function) 
 {
+    if (!IsLowerTriangular(a))
+        throw std::invalid_argument("Matrix A must be lower triangular since only explicit solver is suported");
+    if (b.size() != a.rows() || c.size() != a.rows())
+        throw std::invalid_argument("Vectors b and c must have the same size as the number of rows of matrix A");
     this->a = a;
     this->b = b;
     this->c = c;
@@ -42,20 +47,22 @@ Eigen::MatrixXd RungeKutta::Solve()
     //solve the Runge-Kutta method
     
     auto y0 = initial_condition;
-    Eigen::MatrixXd approximations = Eigen::MatrixXd::Zero(y0.size(), (int)((final_time - initial_time) / step_size) + 1);
+    Eigen::MatrixXd approximations = Eigen::MatrixXd::Zero(y0.col(0).size(), (int)((final_time - initial_time) / step_size) + 1);
     int current_col= 0;    
-    approximations.col(current_col) = y0;
+    approximations.col(current_col) = y0.col(0);
     current_col++;
+    int n_max = approximations.cols();
 
-    for (double t = initial_time; t < (final_time - step_size); t += step_size)
+    //for (double t = initial_time + step_size; t < (final_time+step_size); t += step_size)
+    for (int n = 1; n < n_max; n++)
     {
         int s = b.size();
-        Eigen::MatrixXd k = Eigen::MatrixXd::Zero(y0.size(), s);
+        Eigen::MatrixXd k = Eigen::MatrixXd::Zero(y0.col(0).size(), s);
 
         for (int i = 0; i < s; i++)
         {
-            double time_step = t + c(i) * step_size;
-            Eigen::VectorXd y_step = y0;
+            double time_step = (initial_time + (n-1)*step_size) + c(i) * step_size;
+            Eigen::VectorXd y_step = approximations.col(current_col - 1);
             //std::cout << "y_step: " << y_step.size() << std::endl << std::flush;
             for (int j = 0; j < i; j++)
             {
@@ -68,10 +75,8 @@ Eigen::MatrixXd RungeKutta::Solve()
             k.col(i) = new_rhs;
             //k.row(i) = function.BuildRightHandSide(time_step, y_step);
         }
-
-        auto y1 = y0 + step_size * k * b;
+        auto y1 = approximations.col(current_col - 1) + step_size * k * b;
         approximations.col(current_col) = y1;
-        y0 = y1;
         current_col++;
 
     }
